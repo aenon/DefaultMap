b093f11a0ac01d5e67aa262ec94570f532a0d554
Xilin Sun <s.sn.giraffe@gmail.com>
2018-02-15 14:20:50 -0800

    replace defaultdict with defaultObj code

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
-/*!
- * set
- * Jack Dubie
- */
-
-var _ = require('underscore');
-
-/**
- * Expose `DefaultDict`.
- */
-
-exports = module.exports = DefaultDict;
-
-/**
- * Setup defaults with a `defaultValue`
- *
- * @example
- *   var dict = new DefaultDict(function() { return []; });
- * 
- * @param {function} defaultValue
- * @api public
- */
-
-function DefaultDict(defaultValue) {
-  this.defaultValue = function () {
-    return _.clone(defaultValue);
-  }
-  this._ = {};
+const defaultObj = (defaultValue = undefined) => {
+    const defaultObj = {}
+    const set = (key, value) => {
+        defaultObj[key] = value
+    }
+    const get = key => {
+        if (key in defaultObj) {
+            return defaultObj[key]
+        }
+        return defaultValue
+    }
+    return {set, get}
 }
 
-/**
- * Set `key` to `value`
- *
- * @param {*} key
- * @param {*} value
- * @api public
- */
-
-DefaultDict.prototype.set = function(key, value) {
-  this._[key] = value;
-}
-
-/**
- * Get `value` for `key` if defined. Otherwise return default
- *
- * @param {*} key
- * @return {*}
- * @api public
- */
-
-DefaultDict.prototype.get = function(key, value) {
-  var result = this._[key];
-  if (typeof result !== 'undefined') return result;
-
-  // set this value and recurse
-  this.set(key, this.defaultValue());
-  return this.get(key);
-}
+export default defaultObj
