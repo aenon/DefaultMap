23703f43abe7aee9e6a85d8a91d94ec0a435ff94
Xilin Sun <s.sn.giraffe@gmail.com>
2018-02-15 23:03:10 -0800

    Update build.js

--- build.js
+++ build.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var defaultObj = function defaultObj() {
+var DefaultMap = function DefaultMap() {
     var defaultValue = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : undefined;
 
-    var defaultObj = {};
+    var map = new Map();
     var set = function set(key, value) {
-        defaultObj[key] = value;
+        map.set(key, value);
     };
     var get = function get(key) {
-        if (key in defaultObj) {
-            return defaultObj[key];
+        if (map.get('a')) {
+            return map.get('a');
         }
         return defaultValue;
     };
     return { set: set, get: get };
 };
 
-exports.default = defaultObj;
+exports.default = DefaultMap;
