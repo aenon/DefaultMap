4494144494eed1965b2ed95e2c1d957603e0dd8a
Xilin Sun <s.sn.giraffe@gmail.com>
2018-02-24 22:34:39 -0800

    update index.js: minor fix

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const DefaultMap = (defaultValue = undefined) => {
     const map = new Map()
-    const set = (key, value) => {
-        map.set(key, value)
-    }
-    const get = key => {
-        if (map.get('a')) {
-            return map.get('a')
-        }
-        return defaultValue
-    }
+    const set = (key, value) => {map.set(key, value)}
+    const get = key => map.get(key) || defaultValue 
     return {set, get}
 }
 
